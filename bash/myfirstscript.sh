b217e9f0d4ff1786916081c4b2c7663a9e428145
smit2mw <smit2mw@mail.uc.edu>
2021-01-24 21:15:38 -0500

    My First Commit

---
+++ bash/myfirstscript.sh
@@ -0,0 +1,19 @@
+
+
+#!/bin/bash
+# This script outputs the IP address and the hostname of a machine
+
+greeting="This is a script. Hello!"
+echo $greeting, thanks for joining us!
+echo '$greeting, blah'
+echo "$greeting,hfhfhf \$20"
+echo "      "
+echo "     "
+
+
+echo Machine Type: $MACHTYPE
+echo Hostname: $HOSTNAME
+echo Working Dir: $PWD
+
+
+
