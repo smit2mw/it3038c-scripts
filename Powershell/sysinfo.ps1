61d2b149a397d53a0f09b22bf53c49bbb9b59b8a
Matthew Smith <smit2mw@mail.uc.edu>
2021-01-31 16:27:42 -0500

    adding powershell files

---
+++ Powershell/sysinfo.ps1
@@ -0,0 +1,25 @@
+﻿
+#$ = "Hello, PowerShell!"
+#write-Host($Hello)
+function getIP {
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+}
+
+#write-host(getIP)
+$IP = getIP
+$User = $env:USERNAME
+$HostName = hostname
+$Power = $PSVersionTable.PSVersion | sort-object major | ForEach-Object {$_.major}
+$Date = Get-Date -Format "dddd, MMMM, dd, yyyy"
+
+$Body = "This Machine's IP is $IP."
+#Write-Host("This Machine's IP is $IP.") -NoNewline
+#Write-Host("This Machine's IP is {0}" -f $IP)
+
+#write-host(" User is $User.") -NoNewline
+
+Write-Host(" Hostname is $HostName.") -NoNewline
+
+Write-Host(" PowerShell Version is $Power.") -NoNewline
+
+Write-Host(" Today's Date is $Date") 
