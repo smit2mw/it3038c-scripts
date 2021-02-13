0fd2f476654d01b742fddf538f563525b0b171d5
smit2mw <smit2mw@mail.uc.edu>
2021-02-13 17:49:17 -0500

    Adding Lab5.py

---
+++ Labs/Lab5.py
@@ -0,0 +1,31 @@
+def countCharacterType(str): 
+    
+    vowels = 0
+    consonant = 0
+    totalLetters = 0 
+
+    for i in range(0, len(str)):  
+          
+        ch = str[i]  
+  
+        if ( (ch >= 'a' and ch <= 'z') or 
+             (ch >= 'A' and ch <= 'Z') ):  
+  
+            # To change upper case to lower case for shorter code 
+            ch = ch.lower() 
+  
+            if (ch == 'a' or ch == 'e' or ch == 'i' 
+                        or ch == 'o' or ch == 'u'): 
+                vowels += 1
+                totalLetters +=1
+            else: 
+                consonant += 1
+                totalLetters +=1
+          
+
+    print("Vowels:", vowels) 
+    print("Consonant:", consonant)  
+    print("Total Letters:", totalLetters)
+  
+str = input("Enter you word or phrase: ")
+countCharacterType(str)
