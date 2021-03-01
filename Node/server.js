b467169a5300a91c1c47221bbec6ad11efe7334d
smit2mw <smit2mw@mail.uc.edu>
2021-02-28 22:37:33 -0500

    more

--- Node/server.js
+++ Node/server.js
@@ -13,24 +13,19 @@ var server = http.createServer(function(req, res) {
 
     else if(req.url.match("/sysinfo")) {
         myHostName=os.hostname();
-        String.prototype.toHHMMSS = function () {
-            var sec_num = parseInt(this, 10); // don't forget the second param
-            var hours   = Math.floor(sec_num / 3600);
-            var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-            var seconds = sec_num - (hours * 3600) - (minutes * 60);
-        
-            if (hours   < 10) {hours   = "0"+hours;}
-            if (minutes < 10) {minutes = "0"+minutes;}
-            if (seconds < 10) {seconds = "0"+seconds;}
-            var time    = hours+':'+minutes+':'+seconds;
-            return time;
-        }
-        
-        if(commandCheck("/uptime")){
-            var time = process.uptime();
-            var uptime = (time + "").toHHMMSS();
-            console.log(uptime);
-        }
+        function format(seconds){
+            function pad(s){
+              return (s < 10 ? '0' : '') + s;
+            }
+            var hours = Math.floor(seconds / (60*60));
+            var minutes = Math.floor(seconds % (60*60) / 60);
+            var seconds = Math.floor(seconds % 60);
+          
+            return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+          }
+          
+          var uptime = process.uptime();
+          console.log(format(uptime));
         html=`
         <!DOCTYPE html>
         <head>
