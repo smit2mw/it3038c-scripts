6f3aafc05d8928f3a9fe72355fa3ab7c0f611f31
smit2mw <77588275+smit2mw@users.noreply.github.com>
2021-03-14 21:08:34 -0400

    Update Project2.sh

--- Labs/Project2.sh
+++ Labs/Project2.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# This script is meant to be run on a windows computer even though it is written in BASH
 echo "You are signed in as $(id -u -n)"
 echo
 echo "Your computer's name is $(uname -n)"
