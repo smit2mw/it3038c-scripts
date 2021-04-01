27a2c1ff626988fd8526bbc6200de859bd8bdcf1
smit2mw <smit2mw@mail.uc.edu>
2021-04-01 11:26:56 -0400

    Lab09commit

---
+++ Node/API.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var data = require("C:/Users/bronz/it3038c-scripts/widgets.json");
+
+var server = http.createServer(function(req, res){
+    if (req.url === "/") {
+        res.writeHead(200, {"Content-Type": "text/json"});
+        res.end(JSON.stringify(data));
+    }
+else {
+        res.writeHead(404, {"Content-Type": "text/plain"});      
+        res.end("Data not found");        
+    }
+});
+
+server.listen(3000);
+console.log("Server is listening on port 3000");
