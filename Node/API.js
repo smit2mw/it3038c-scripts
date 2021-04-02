a387a35afe5c4e00d5d7e24727a8c0a3809b0350
smit2mw <smit2mw@mail.uc.edu>
2021-04-02 17:38:06 -0400

    commit

--- Node/API.js
+++ Node/API.js
@@ -6,11 +6,23 @@ var server = http.createServer(function(req, res){
         res.writeHead(200, {"Content-Type": "text/json"});
         res.end(JSON.stringify(data));
     }
-else {
+    else if (req.url === "/blue") {
+        listBlue(res);
+    
+    }
+    else {
         res.writeHead(404, {"Content-Type": "text/plain"});      
         res.end("Data not found");        
     }
 });
+
+function listBlue(res) {
+    var colorBlue = data.filter(function(item) {
+        return item.color === "blue";
+    });
+
+    res.end(JSON.stringify(colorBlue));
+}
 
 server.listen(3000);
 console.log("Server is listening on port 3000");
