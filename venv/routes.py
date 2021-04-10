5ee3cb760e0ab0d776a10da039ef53e4f7ddd9b5
smit2mw <smit2mw@mail.uc.edu>
2021-04-10 16:44:30 -0400

    flask final

--- venv/routes.py
+++ venv/routes.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 @app.route('/')
 def hello():
-   name = 'Matthew'
-   return render_template("index.html", name=name)
+   #name = 'Matthew'
+   return render_template("index.html")
+   #return render_template("index.html", name=name)
+   #, name=myName)
 
 
-
-@app.route('/welcome')
+@app.route('/welcome', methods=['POST'])
 def welcome():
-   return render_template("welcome.html")
+   return render_template("welcome.html", myName=request.form['myName'])
