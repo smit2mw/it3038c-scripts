c8fb4ce8882a07ac2b4a1db9ec085032b0383f22
smit2mw <smit2mw@mail.uc.edu>
2021-04-10 16:36:09 -0400

    commit

--- venv/routes.py
+++ venv/routes.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 @app.route('/')
 def hello():
-   return 'Hello, World!'
+   name = 'Matthew'
+   return render_template("index.html", name=name)
+
 
 
 @app.route('/welcome')
