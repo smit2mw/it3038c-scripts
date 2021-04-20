7e401dedc647f4413763300d364672ce165f5671
smit2mw <smit2mw@mail.uc.edu>
2021-04-20 15:58:06 -0400

    Project 3 update

--- Labs/Project3/Text2Speech.py
+++ Labs/Project3/Text2Speech.py
@@ -3,8 +3,12 @@ import os
 
 
 langauge = 'EN'             #Sets the language
-yourText = "Hello Daddy Senpai."
+yourText = input("Enter your text:  ")
 
 output = gTTS(text=yourText, lang=langauge, slow=False) 
 #gTTS expects this naming scheme so I am passing it my two variables
 #gTTS has two speeds of playback: Fast and Slow. I want Fast.
+
+output.save("Text2Speech.mp3") #Saves the output as mp3 file on your computer
+
+os.system("start Text2Speech.mp3") #Plays the afore mentioned audio file
