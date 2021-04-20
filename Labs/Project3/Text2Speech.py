2b13c814b988eb1246d07f91f1b6cc3fd70f09fd
smit2mw <smit2mw@mail.uc.edu>
2021-04-20 16:09:43 -0400

    Project3 Final

--- Labs/Project3/Text2Speech.py
+++ Labs/Project3/Text2Speech.py
@@ -1,9 +1,12 @@
-from gtts import gTTS
+from gtts import gTTS   #Google's Text to Speech API
 import os
 
 
-langauge = 'en'             #Sets the language
+langauge = 'en' 
+#Sets the language
+
 yourText = input("Enter your text:  ")
+#User enters the Text they'd like to hear
 
 output = gTTS(text=yourText, lang=langauge, slow=False) 
 #gTTS expects this naming scheme so I am passing it my two variables
